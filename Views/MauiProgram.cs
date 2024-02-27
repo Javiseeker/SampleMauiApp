8e490aa9bdf452b994e98ec63e6bcf380d071a63
Javier AlonsoL <javier.alonsol@perficient.com>
2024-02-27 10:51:43 -0500

    FEAT: added changes to the mauiprogram for lifecyle app event

--- Views/MauiProgram.cs
+++ Views/MauiProgram.cs
@@ -4,15 +4,14 @@ using Microsoft.Extensions.Logging;
 using Microsoft.Maui.LifecycleEvents;
 using ViewModels;
 using Views.Pages;
-#if WINDOWS
-using Views.WinUI;
-#endif
+
 namespace Views
 {
     public static class MauiProgram
     {
         public static MauiApp CreateMauiApp()
         {
+
             var builder = MauiApp.CreateBuilder();
             builder
                 .UseMauiApp<App>()
@@ -20,10 +19,23 @@ namespace Views
                 .UseMauiCommunityToolkit()
                 .ConfigureLifecycleEvents(events =>
                 {
-                    //events.AddWindows(windowsLifecycleBuilder =>
-                    //{
-                    
-                    //});
+#if WINDOWS
+                    events.AddWindows(windowsLifecycleBuilder =>
+                    {
+                        windowsLifecycleBuilder.OnWindowCreated((window) =>
+                        {
+                            var handle = WinRT.Interop.WindowNative.GetWindowHandle(window);
+                            var id = Microsoft.UI.Win32Interop.GetWindowIdFromWindow(handle);
+                            var appWindow = Microsoft.UI.Windowing.AppWindow.GetFromWindowId(id);
+
+                            appWindow.Closing += (sender, e) =>
+                            {
+                                e.Cancel = true;
+                                window.Close();
+                            };
+                        });
+                    });
+#endif
                 })
                 .ConfigureFonts(fonts =>
                 {
