8204e5485a73786ede8b0b007ebd3227b5b01c21
Javier AlonsoL <javier.alonsol@perficient.com>
2024-02-28 11:31:20 -0500

    FEAT: added 2 changes to show data bindings approaches in .NET MAUI

--- ViewModels/FromScratchViewModel.cs
+++ ViewModels/FromScratchViewModel.cs
@@ -1,12 +1,79 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System.Collections.ObjectModel;
 
-namespace ViewModels
+namespace ViewModels;
+
+[ObservableRecipient]
+public partial class FromScratchViewModel: ObservableObject // , INotifyPropertyChanged
 {
-    public class FromScratchViewModel
+    #region MAUI Community Toolkit
+    [ObservableProperty]
+    private ObservableCollection<string> collectionBindedWithToolkit;
+
+    #endregion
+
+    #region MAUI Native Data Binding with MVVM
+    private ObservableCollection<string> _collectionBindedNatively;
+    public ObservableCollection<string> CollectionBindedNatively
     {
+        get => _collectionBindedNatively;
+        set
+        {
+            if (_collectionBindedNatively != value)
+            {
+                _collectionBindedNatively = value;
+                OnPropertyChanged(nameof(CollectionBindedNatively));
+            }
+        }
+    }
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    protected virtual void OnPropertyChanged(string propertyName)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+    #endregion
+
+    public FromScratchViewModel()
+    {
+        CollectionBindedNatively = new();
+        CollectionBindedWithToolkit = new();
+    }
+    public async Task InitializeView()
+    {
+        await MainThread.InvokeOnMainThreadAsync(() =>
+        {
+            CollectionBindedNatively.Add("Test");
+            CollectionBindedNatively.Add("Test2");
+            CollectionBindedNatively.Add("Test3");
+            CollectionBindedNatively.Add("Test4");
+
+            CollectionBindedWithToolkit.Add("Test");
+            CollectionBindedWithToolkit.Add("Test2");
+            CollectionBindedWithToolkit.Add("Test3");
+            CollectionBindedWithToolkit.Add("Test4");
+        });
+    }
+
+    public async Task PushToCV()
+    {
+        Random rand = new Random();
+        var valueToAdd = rand.Next(5, 100);
+        await MainThread.InvokeOnMainThreadAsync(() =>
+        {
+            CollectionBindedNatively.Add($"TRad{valueToAdd}");
+            CollectionBindedWithToolkit.Add($"TRad{valueToAdd}");
+        });
+    }
+
+    public async Task PopFromCV()
+    {
+        if (CollectionBindedNatively.Any())
+            await MainThread.InvokeOnMainThreadAsync(() =>
+            {
+                CollectionBindedNatively.RemoveAt(CollectionBindedNatively.Count - 1);
+                CollectionBindedWithToolkit.RemoveAt(CollectionBindedNatively.Count - 1);
+            });
     }
 }
--- Views/Pages/FromScratchView.cs
+++ Views/Pages/FromScratchView.cs
@@ -13,35 +13,98 @@ public class FromScratchView : ContentPage
 		_fromScratchViewModel = fromScratchViewModel;
 		BindingContext = _fromScratchViewModel;
 
-        var dataTemplate = new DataTemplate()
+
+
+        #region MAUI Native Data Binding with MVVM
+        var collectionView = new CollectionView() { HeightRequest = 295 };
+
+        collectionView.SetBinding(CollectionView.ItemsSourceProperty, "CollectionBindedNatively", BindingMode.Default);
+
+        collectionView.ItemTemplate = new DataTemplate(() =>
         {
+            var label = new Label() { Padding = new Thickness(20, 2, 0, 0) };
+            label.SetBinding(Label.TextProperty, new Binding("."));
+            return label;
+        });
+        #endregion
 
-        };
+        #region MAUI Community Toolkit
+        var collectionView2 = new CollectionView();
+        collectionView2.Bind(CollectionView.ItemsSourceProperty, "CollectionBindedWithToolkit", BindingMode.Default);
 
-        var collectionView = new CollectionView();
+        collectionView2.ItemTemplate = new DataTemplate(() =>
+        {
+            var label = new Label() { Padding = new Thickness(20, 2, 0, 0) };
+            label.SetBinding(Label.TextProperty, new Binding("."));
+            return label;
+        });
+        #endregion
 
-		// MAUI native binding
-		//collectionView.SetBinding();
+        var onPushGesture = new TapGestureRecognizer();
+        onPushGesture.Tapped += onPushBtnClick;
 
-
-		// Using Community ToolKit
-		//collectionView.Bind();
-        
-		
+        var onPopGesture = new TapGestureRecognizer();
+        onPopGesture.Tapped += onPopBtnClick;
 
         Content = new VerticalStackLayout
 		{
 			Spacing = 20,
 			Children = {
 				new Label { HorizontalOptions = LayoutOptions.Center, VerticalOptions = LayoutOptions.Center, Text = "Welcome to .NET MAUI FROM SCRATCH!!" },
-				collectionView
-			}
-		};
+				new HorizontalStackLayout
+				{
+					Spacing = 5,
+					Children =
+					{
+                        new Button { Text="Stack Layout" },
+                        new Button { Text="Horizontal Stack Layout" },
+                        new Button { Text="Vertical Stack Layout" },
+                        new Button { Text="Absolute Layout" },
+                        new Button { Text="Flex Layout" },
+                        new Button { Text="Grid" },
+                    }
+                    
+				}.Center(),
+                new HorizontalStackLayout
+                {
+                    Spacing = 20,
+                    Children =
+                    {
+                        new Button { Text="Push", GestureRecognizers =
+                            {
+                                onPushGesture
+                            }
+                        },
+                        new Button { Text="Pop", GestureRecognizers =
+                            {
+                                onPopGesture
+                            }
+                        }
+                    }
+
+                }.Center(),
+                new Border {HeightRequest = 315, WidthRequest= 100,Content = collectionView.Margins(5,5,5,0)}.Center(),
+                //new Border {HeightRequest = 315, WidthRequest= 100,Content = collectionView2.Margins(5,5,5,0)}.Center()
+
+            }
+        };
 	}
+
+    private async void onPushBtnClick(object? sender, TappedEventArgs e)
+    {
+        await _fromScratchViewModel.PushToCV();
+    }
+
+    private async void onPopBtnClick(object? sender, TappedEventArgs e)
+    {
+        await _fromScratchViewModel.PopFromCV();
+    }
 
     protected override async void OnAppearing()
     {
 		base.OnAppearing();
+
+		await _fromScratchViewModel.InitializeView();
     }
     protected override void OnDisappearing()
     {
